8b8f0a4a42d5e596356a131cbcccee0b51bfd18b
坂本のぞみ <sakamotonozomi@sakamotonozomi-no-MacBook-Air.local>
2016-12-08 16:16:20 +0900

    Initial Commit

---
+++ Sample.Alert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample.Alert
+//
+//  Created by 坂本のぞみ on 2016/12/08.
+//  Copyright © 2016年 坂本のぞみ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
